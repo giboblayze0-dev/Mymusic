4b4608d42442a1d67aae3767d79b9e07c31881f8
Gibo Blayze <giboblayze0@gmail.com>
2026-03-22 18:39:49 +0200

    https://github.com/giboblayze0-dev/Mymusic.website.git

---
+++ main.js
@@ -0,0 +1,9 @@
+function showMore(){
+  var more = document.getElementById("moreSongs");
+
+  if(more.style.display === "block"){
+    more.style.display = "none";
+  } else {
+    more.style.display = "block";
+  }
+}
